f5d7d84db2baf0da32dd983cf5df4015d224defd
SC00367807 <SC00367807@TechMahindra.com>
2017-06-15 16:34:26 +0530

    New files

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,20 @@
+/*
+* VehSchPOC : NewFunc7 : About Page : SC00367807 : 15Jun17
+*/
+
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'about',
+  template: `
+  <md-card>
+    <div align = "center">
+      <h1>About Page</h1>
+      This is a mock page for About. 
+    </div>
+    </md-card>
+  `
+})
+export class AboutComponent {
+
+}
